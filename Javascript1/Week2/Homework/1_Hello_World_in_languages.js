859cbe8e06f2066842785cded2f63c3bd8f134fc
A T <toegelanna@outlook.com>
2019-07-03 17:07:25 +0200

    ex1

---
+++ Javascript1/Week2/Homework/1_Hello_World_in_languages.js
@@ -0,0 +1,5 @@
+let hello = ['Halo dunia' /* Indonesian*/, 'Ciao mondo' /* Italian*/, 'Hola mundo' /*Spanish*/]
+let greet = hello; 
+console.log(greet[0]);
+console.log(greet[1]);
+console.log(greet[2]);
