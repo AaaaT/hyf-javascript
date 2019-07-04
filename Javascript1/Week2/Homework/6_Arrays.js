ca243347c6f99deccb87f55ff65b6c21e8e4bf84
A T <toegelanna@outlook.com>
2019-07-04 17:35:19 +0200

    ex6

---
+++ Javascript1/Week2/Homework/6_Arrays.js
@@ -0,0 +1,22 @@
+// 6. Arrays
+// 6.1 Declare an empty array. Make sure that the name you choose indicates 'plurality', because an array is capable of containing more than one element. (See Naming conventions).
+let apples = [];
+
+// 6.2 Write a console.log statement that explains in words what you think the value of the array is.
+console.log('the value of the array will be object');
+
+// 6.3 console.log your array.
+console.log(apples);
+
+// 6.4 Create an array that has your favorite animals inside (see if you can find a good name that exactly describes what this variable will hold).
+let myFavAnimals = ['cat', 'dog', 'bunny', 'horse'];
+
+// 6.5 Log your array.
+console.log(myFavAnimals);
+
+// 6.6 Add a statement that adds Daan's favorite animal ('baby pig') to the existing array.
+//example ---> var animals = ['pigs', 'goats', 'sheep']; console.log(animals.push('cows')); expected output: 4
+myFavAnimals.push('baby pig');
+
+// 6.7 Log your new array!
+console.log(myFavAnimals);
