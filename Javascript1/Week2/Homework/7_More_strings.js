ab4ab60453f5f622b127df6bda2e2779f2611776
A T <toegelanna@outlook.com>
2019-07-04 17:50:47 +0200

    ex7

---
+++ Javascript1/Week2/Homework/7_More_strings.js
@@ -0,0 +1,14 @@
+// 7. More strings
+// Let's consider the following string: let myString = "this is a test".
+let myString = "this is a test"
+
+// 7.1 Add the string to your file and console.log it.
+console.log(myString);
+
+// 7.2 Find a way to get the length of myString.
+myString.length
+
+// 7.3 console.log the length of myString.
+console.log(myString.length);
+
+
