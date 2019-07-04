1377e8ff8d90164e6910a8b230b62dbd52d394e1
A T <toegelanna@outlook.com>
2019-07-04 13:49:29 +0200

    structure

---
+++ Javascript1/Week2/Homework/1_Hello_World_in_Languages_version1.js
@@ -0,0 +1,4 @@
+console.log('Hello World! //english ')
+console.log('Hallo die Welt //german')
+console.log('Hej Världen //swedish')
+
---
+++ Javascript1/Week2/Homework/week3test.js
@@ -0,0 +1,31 @@
+const ass = require ('assert')
+const assert = ass.deepEqual
+p = console.log
+const print = p
+print ('=================')
+
+//f = (x) => type
+const x=1
+print(x)
+
+class Teacher {
+    name;
+    age;
+    subject;
+}
+
+student = {
+    name: 'Bo',
+    age: 32,
+
+    say Hello:() => console.log ('Hello!'),
+}
+
+student.name
+student['name']
+student.age
+student.sayHello()
+
+''.toUpperCase
+
+console.log('Hello World')
