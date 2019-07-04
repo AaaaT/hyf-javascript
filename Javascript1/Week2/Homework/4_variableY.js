ea95eb8803630ad965e7d2f3b3c665995f9be02a
A T <toegelanna@outlook.com>
2019-07-04 16:04:46 +0200

    ex4

---
+++ Javascript1/Week2/Homework/4_variableY.js
@@ -0,0 +1,27 @@
+// 4. Declare a variable y and assign a string to it.
+let y = 'hi'; //declaring and assigning
+
+// 4.1 Write a console.log statement in which you explain in words what you think the value of the string is.
+
+//console.log(y)("the value of y will be string");
+console.log(y);
+console.log('the value of y will be ' +  typeof(y));
+
+// 4.2 Now console.log the variable y.
+console.log(y);
+
+// 4.3 Now assign a new string to the variable y.
+y = 'hello'; //asigning
+
+// 4.4 Just like what you did before write a console.log statement that explains in words what you think will be logged to the console.
+//console.log('the value of y will be hi and hello'); --->wrong the value changes to hello
+
+// 4.5 Now console.log y again.
+console.log(y);
+
+// // TODO -> here you declare AND assign your string
+// console.log("the value of my string will be: whateverYouThinkItWillLog");
+// // TODO -> log the actual value of the string to the console
+// // TODO -> assign a new value to your variable x
+// console.log("the value of my string will be: whateverYouThinkItWillLog");
+// // TODO -> log the actual value of the string to the console
