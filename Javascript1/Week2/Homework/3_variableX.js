733e783be7d1a19b6613892714832c71fc71adbb
A T <toegelanna@outlook.com>
2019-07-04 14:25:11 +0200

    ex3

--- Javascript1/Week2/Homework/3_variableX.js
+++ Javascript1/Week2/Homework/3_variableX.js
@@ -2,18 +2,28 @@
 //3.1 First, declare your variable x (do not initialize it yet).
 let x
 //3.2 Add a console.log statement that explains in words what you think the value of x is, like in this example:
-
 // // TODO -> here you initialize your variable
+console.log("the value of my variable x will be: whateverYouThinkItWillLog");
+console.log("the value of x will be undefined because we have not assigned it yet")
+
 // 3.3 Add a console.log statement that logs the value of x.
 // console.log("the value of my variable x will be: whateverYouThinkItWillLog");
+console.log(x);
+
 // 3.4 Now initialize your variable x with an integer.
+x=7
+
 // 3.5 Next, add a console.log statement that explains what you think the value of x is.
+console.log(7); 
+
 // 3.6 Add a console.log statement that logs the value of x.
 // Steps to be taken:
-
 // // TODO -> here you declare your variable
 // console.log("the value of x will be: whateverYouThinkItWillLog");
 // // TODO -> log the actual value of x
 // // TODO -> here you initialize your variable
 // console.log("the value of x will be: whateverYouThinkItWillLog");
 // // TODO -> log value of x again
+
+console.log("the value of x will be" 7);
+console.log(x);
