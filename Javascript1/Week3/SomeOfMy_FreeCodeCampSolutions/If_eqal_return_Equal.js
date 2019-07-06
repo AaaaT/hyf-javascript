29ac7fd1a8b31120e26db772168850e29ae5c665
AaaaT <toegelanna@outlook.com>
2019-07-06 22:35:37 +0200

    Update If_eqal_return_Equal.js

--- Javascript1/Week3/SomeOfMy_FreeCodeCampSolutions/If_eqal_return_Equal.js
+++ Javascript1/Week3/SomeOfMy_FreeCodeCampSolutions/If_eqal_return_Equal.js
@@ -1,6 +1,6 @@
 // There are many Comparison Operators in JavaScript. All of these operators return a boolean true or false value.
 // The most basic operator is the equality operator ==. The equality operator compares two values and returns true if they're equivalent or false if they are not. 
-// Note that equality is different from assignment (=), which assigns the value at the right of the operator to a variable in the left.
+// **Note that equality is different from assignment (=), which assigns the value at the right of the operator to a variable in the left.**
 
 // Setup
 function testEqual(val) {
