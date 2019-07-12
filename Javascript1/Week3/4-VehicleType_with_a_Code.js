2d4430e2657b939f5dc57b7821b9b13833d9aeef
A T <toegelanna@outlook.com>
2019-07-12 16:14:31 +0200

    ex 4 vehicle code

---
+++ Javascript1/Week3/4-VehicleType_with_a_Code.js
@@ -0,0 +1,21 @@
+//Create a function named vehicleType that receives a color, 
+//and a code, 1 for car, 2 for motorbike. And prints 'a blue motorbike' 
+//for example when called as vehicleType("blue", 2)
+
+
+
+
+
+function vehicleType(color, code){
+    let type; 
+    if (code == 1){
+        type = "car"
+    }; 
+    if (code == 2){
+        type = "motorbike"
+    };
+ 
+console.log("a " + color + " "+ type)
+};
+
+console.log(vehicleType("blue", 2));
