3f35d954dfb2bb4d4dc6f75a1cb245ba57eff1a8
A T <toegelanna@outlook.com>
2019-07-12 14:45:35 +0200

    red car ex 2

--- Javascript1/Week3/2-A_Red _Car.js
+++ Javascript1/Week3/2-A_Red _Car.js
@@ -1,1 +1,10 @@
 //Create a function named colorCar that receives a color, and prints out, 'a red car' for example.
+
+function colorCar(color){
+   
+    return "a " + color +" car";
+}
+color = "red"
+// color = "blue"
+// color = "green"
+console.log(colorCar(color));
