b52f140f70cabf8d8b26ed6b50b2d2803e55f7b9
A T <toegelanna@outlook.com>
2019-07-12 14:34:52 +0200

    ex1 sum 3 arguments

---
+++ Javascript1/Week3/1-Sum_Of_3_Arguments.js
@@ -0,0 +1,6 @@
+//Create a function that takes 3 arguments and returns the sum of the these arguments.
+
+function threeArguments(arg1, arg2, arg3){
+   return arg1+arg2+arg3
+}
+console.log(threeArguments(5, 6, 10));
---
+++ Javascript1/Week3/2-A_Red _Car.js
@@ -0,0 +1,1 @@
+//Create a function named colorCar that receives a color, and prints out, 'a red car' for example.
