c0f3af61cfe50b680de2531e690ceb4000edf907
unknown <toegelanna@outlook.com>
2019-08-06 23:32:23 +0200

    experimentin with a simple greeting function and a little html button

---
+++ function_AaaaT_Greeting/script.js
@@ -0,0 +1,13 @@
+// Code goes here
+
+
+
+function greeting(name, surname) {
+
+  console.log('hi' + ' ' + name + ' ' + surname)
+}
+
+
+greeting('Aaaa', 'T');
+greeting('Jane', 'Austin');
+greeting('Jack', 'London');
